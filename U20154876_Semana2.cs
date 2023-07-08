312b5b2988947c59eb2d5046477eff4943235b2d
Roxan97 <soyingrid.1977@gmail.com>
2023-07-08 17:34:19 -0600

    Declaracion de variables

---
+++ U20154876_Semana2.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DECLARACION DE VARIABLES
+
+            //ITEMS 1
+
+            int edad= 30;
+
+            string objeto="computadora";
+
+            // ITEMS 2
+
+            float salario=502.25;
+
+            decimal precio = 20.3658;
+
+            //ITEMS 3
+
+            bool activo=true;
+
+
+            //AUTOEVALUACION 10,9,9.5 
+
+
+
+
+           
+
+
+        }
+        
+
+
+
+
+    }
+}
